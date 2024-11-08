5d1adfaad4e0be3bdda8da666b0ca715bd13d978
fazlullinhub <164046064+fazlullinhub@users.noreply.github.com>
2024-11-08 11:30:52 +0300

    Add files via upload

---
+++ ConsoleApp47/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp47
+{
+    public class MinStack
+    {
+        private Stack<int> stack;
+        private Stack<int> minStack;
+
+        public MinStack()
+        {
+            stack = new Stack<int>();
+            minStack = new Stack<int>();
+        }
+
+        public void Push(int value)
+        {
+            stack.Push(value);
+
+            if (minStack.Count == 0 || value <= minStack.Peek())
+            {
+                minStack.Push(value);
+            }
+        }
+
+        public int Pop()
+        {
+            if (stack.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+
+            int value = stack.Pop();
+
+            if (value == minStack.Peek())
+            {
+                minStack.Pop();
+            }
+            return value;
+        }
+
+        public int Top()
+        {
+            if (stack.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+
+            return stack.Peek();
+        }
+
+        public int GetMin()
+        {
+            if (minStack.Count == 0)
+            {
+                throw new InvalidOperationException("The stack is empty.");
+            }
+
+            return minStack.Peek();
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MinStack minStack = new MinStack();
+
+            minStack.Push(3);
+            minStack.Push(5);
+            Console.WriteLine("Минимальный элемент: " + minStack.GetMin());
+            minStack.Push(2);
+            minStack.Push(1);
+            Console.WriteLine("Минимальный элемент: " + minStack.GetMin());
+            minStack.Pop();
+            Console.WriteLine("Минимальный элемент: " + minStack.GetMin());
+            minStack.Pop();
+            Console.WriteLine("Вершина стека: " + minStack.Top()); 
+            Console.WriteLine("Минимальный элемент: " + minStack.GetMin());
+        }
+    }
+}
